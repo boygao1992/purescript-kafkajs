de71176993f718ddcd277e377ae6a58bb1a78575
boygao1992 <wg2313@columbia.edu>
2022-12-12 15:39:29 -0500

    add `Kafka.Consumer.subscribe`

--- src/Kafka/Consumer.js
+++ src/Kafka/Consumer.js
@@ -3,3 +3,7 @@
 exports._consumer = function _consumer(kafka, config) {
   return kafka.consumer(config);
 };
+
+exports._subscribe = function _subscribe(consumer, subscription) {
+  return consumer.subscribe(subscription);
+};
--- src/Kafka/Consumer.purs
+++ src/Kafka/Consumer.purs
@@ -1,14 +1,22 @@
 module Kafka.Consumer
   ( Consumer
   , ConsumerConfig
+  , Topic(..)
   , consumer
+  , subscribe
   ) where
 
 import Prelude
 
+import Control.Promise as Control.Promise
+import Data.Maybe as Data.Maybe
+import Data.String.Regex as Data.String.Regex
 import Effect as Effect
+import Effect.Aff as Effect.Aff
 import Effect.Uncurried as Effect.Uncurried
+import Kafka.FFI as Kafka.FFI
 import Kafka.Kafka as Kafka.Kafka
+import Untagged.Union as Untagged.Union
 
 -- | https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/types/index.d.ts#L1032
 foreign import data Consumer :: Type
@@ -39,6 +47,37 @@ type ConsumerConfig =
 type ConsumerConfigImpl =
   { groupId :: String }
 
+type ConsumerSubscribeTopics =
+  { fromBeginning :: Data.Maybe.Maybe Boolean
+  , topics :: Array Topic
+  }
+
+-- | https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/types/index.d.ts#L1030
+-- |
+-- | Required
+-- | * `topics: (string | RegExp)[]`
+-- |
+-- | Optional
+-- | * `fromBeginning?: boolean`
+-- |   * default to `false`, see https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/src/consumer/index.js#L134
+type ConsumerSubscribeTopicsImpl =
+  Kafka.FFI.Object
+    ( topics :: Array ConsumerSubscribeTopicImpl
+    )
+    ( fromBeginning :: Boolean
+    )
+
+-- | https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/types/index.d.ts#L1030
+-- |
+-- | `(string | RegExp)`
+type ConsumerSubscribeTopicImpl =
+  String
+    Untagged.Union.|+| Data.String.Regex.Regex
+
+data Topic
+  = TopicName String
+  | TopicRegex Data.String.Regex.Regex
+
 -- | https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/types/index.d.ts#L12
 -- |
 -- | `consumer(config: ConsumerConfig): Consumer`
@@ -56,3 +95,36 @@ consumer kafka config =
   toConsumerConfigImpl :: ConsumerConfig -> ConsumerConfigImpl
   toConsumerConfigImpl x = x
 
+-- | https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/types/index.d.ts#L1035
+-- |
+-- | `subscribe(subscription: ConsumerSubscribeTopics | ConsumerSubscribeTopic): Promise<void>`
+-- |  * NOTE `ConsumerSubscribeTopic` is deprecated, see https://github.com/tulios/kafkajs/blob/dcee6971c4a739ebb02d9279f68155e3945c50f7/types/index.d.ts#L1027
+foreign import _subscribe ::
+  Effect.Uncurried.EffectFn2
+    Consumer
+    ConsumerSubscribeTopicsImpl
+    (Control.Promise.Promise Unit)
+
+subscribe ::
+  Consumer ->
+  ConsumerSubscribeTopics ->
+  Effect.Aff.Aff Unit
+subscribe consumer' consumerSubscribeTopics =
+  Control.Promise.toAffE
+    $ Effect.Uncurried.runEffectFn2 _subscribe consumer'
+    $ toConsumerSubscribeTopicsImpl consumerSubscribeTopics
+  where
+  toConsumerSubscribeTopicsImpl ::
+    ConsumerSubscribeTopics ->
+    ConsumerSubscribeTopicsImpl
+  toConsumerSubscribeTopicsImpl x = Kafka.FFI.objectFromRecord
+    { fromBeginning: x.fromBeginning
+    , topics: toConsumerSubscribeTopicImpl <$> x.topics
+    }
+
+  toConsumerSubscribeTopicImpl ::
+    Topic ->
+    ConsumerSubscribeTopicImpl
+  toConsumerSubscribeTopicImpl topic = case topic of
+    TopicName string -> Untagged.Union.asOneOf string
+    TopicRegex regex -> Untagged.Union.asOneOf regex
