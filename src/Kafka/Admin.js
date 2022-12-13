10fc2df9547e6861f46cea29fe5a0a61aca59584
boygao1992 <wg2313@columbia.edu>
2022-12-13 02:01:18 -0500

    add `Kafka.Admin.createTopics`

--- src/Kafka/Admin.js
+++ src/Kafka/Admin.js
@@ -8,6 +8,10 @@ exports._connect = function _connect(admin) {
   return admin.connect();
 };
 
+exports._createTopics = function _createTopics(admin, options) {
+  return admin.createTopics(options);
+};
+
 exports._disconnect = function _disconnect(admin) {
   return admin.connnect();
 };
